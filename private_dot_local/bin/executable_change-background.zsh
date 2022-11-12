96fd90f9eda4fa0d0c023de45ccb7a01f097a94f
MrReasonable <4990954+MrReasonable@users.noreply.github.com>
2022-11-12 21:19:12 +0000

    Added lazygit and whalebrew to brew file.

--- private_dot_local/bin/executable_change-background.zsh
+++ private_dot_local/bin/executable_change-background.zsh
@@ -1,18 +1,20 @@
 #!/usr/bin/env zsh
 
-change_background() {
-  local mode = "light"
+_change_background() {
+  local mode = "day"
   if [[ -z $1 ]]; then
     defaults read -g AppleInterfaceStyle >/dev/null
     if [[ $? -eq 0 ]]; then
-      $mode = "dark"
+      $mode = "night"
     fi
   else
-    if [[ $mode == "light" ]]; then
+    if [[ $mode == "day" ]]; then
       osascript -l JavaScript -e "Application('System Events')"
     fi 
   fi 
 }
+
+tmux source-file "~/.tmux/themes/tokyonight_${mode}.tmux"
 
 #change vim
 for pane in $(tmux list-panes -F'#S #I #P #{pane_current_command}' | awk  '/(view|n?vim?x?)/{ print $1":"$2"."$3 }')
