468dbc58e8f642c3e9f245e456060a05269f2911
MrReasonable <4990954+MrReasonable@users.noreply.github.com>
2023-02-13 08:39:20 +0000

    Updated gitconfig and added some more vim plugins.

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -114,6 +114,9 @@ return packer.startup(function(use)
 
   -- wakatime
   use { 'wakatime/vim-wakatime' }
+
+  -- Zinit plugin syntax highlighting
+  use { 'zdharma-continuum/zinit-vim-syntax' }
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then
