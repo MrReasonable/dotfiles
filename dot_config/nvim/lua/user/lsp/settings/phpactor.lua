ffb800a9dbe43207559743906b0a81b7414dfdc6
MrReasonable <4990954+MrReasonable@users.noreply.github.com>
2023-05-15 09:36:52 +0100

    Switched to NvChad to make nvim easier to manage.

--- dot_config/nvim/lua/user/lsp/settings/phpactor.lua
+++
@@ -1,6 +0,0 @@
-return {
-  init_options = {
-    ["language_server_phpstan.enabled"] = true,
-    ["language_server_psalm.enabled"] = true,
-  }
-}
--- dot_config/nvim/lua/user/virt-column.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, v_column = pcall(require, "virt-column")
-
-if not status_ok then
-  vim.notify("unable to load virt-column")
-  return
-end
-
-v_column.setup()
